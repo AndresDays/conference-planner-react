a239c74330f940fe972eef5fd6a72e3df9e15592
Andres Diaz <jrcasp11@gmail.com>
2025-03-09 19:30:43 -0400

    corregido

--- src/main.jsx
+++ src/main.jsx
@@ -1,18 +1,14 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import App from './App.jsx';
-import './index.css';
-import { Provider } from 'react-redux';
-import store from './store.js';
-import { BrowserRouter } from 'react-router-dom'; // Importa BrowserRouter
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App.jsx'
+import './index.css'
+import { Provider } from 'react-redux'
+import store from './store.js'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <Provider store={store}>
-      {/* Aquí envolvemos la aplicación con BrowserRouter */}
-      <BrowserRouter basename="/conference-planner-react"> {/* Cambia esto por el nombre de tu app o subdirectorio */}
-        <App />
-      </BrowserRouter>
+    <App />
     </Provider>
-  </React.StrictMode>
-);
+  </React.StrictMode>,
+)
